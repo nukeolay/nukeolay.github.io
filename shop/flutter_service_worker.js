350548b826b620f161f7f71d82366b7cecde6a28
nukeolay <66204571+nukeolay@users.noreply.github.com>
2021-12-15 22:07:06 +0300

    shop

--- shop/flutter_service_worker.js
+++ shop/flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dee4252cacdc52bddd2262cbbe5881df",
-"/": "dee4252cacdc52bddd2262cbbe5881df",
+"index.html": "a6bd22d69cec7b3c9459cf2d9a10ef8b",
+"/": "a6bd22d69cec7b3c9459cf2d9a10ef8b",
 "main.dart.js": "c43924523dac1013187f5bd5cf5ee6b7",
 "manifest.json": "e73f3e6f74e820a4144d31d0e8691e87",
 "version.json": "c4e35e29036ee2e58d24753160e6c082"
