9db03bc9d720c99332588c0fa4f588daef2d8e20
nukeolay <66204571+nukeolay@users.noreply.github.com>
2022-01-19 23:17:40 +0300

    backward

--- backward/flutter_service_worker.js
+++ backward/flutter_service_worker.js
@@ -3,10 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "a36e62354abdeb7a5159371ef7cb4ddb",
+"assets/assets/bg.png": "a8bb18dfb9105c43f74272182379443d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "cbb8a596057a839a739f97c6e0925c04",
+"assets/NOTICES": "ce1cbf45c77a04524ba6153e64480bc2",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
 "canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
@@ -17,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e210cf4467a94e52e6bfd500e723a1da",
-"/": "e210cf4467a94e52e6bfd500e723a1da",
-"main.dart.js": "115e85ea5f631b79d735b4e91aa4ac37",
+"index.html": "38eacc043b97cc49b8978e1934e32177",
+"/": "38eacc043b97cc49b8978e1934e32177",
+"main.dart.js": "7657b2f26eaa95864cc9e3a7b32bf03f",
 "manifest.json": "9e5d639b9fcb17e026a1d328270febff",
 "version.json": "7904013235a5c7449c4605d473fee233"
 };
