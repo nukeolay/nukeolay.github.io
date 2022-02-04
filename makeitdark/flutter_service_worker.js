a9a6f32a617ff25741ed5f72483ae7e9fcfc4382
nukeolay <66204571+nukeolay@users.noreply.github.com>
2022-02-04 11:04:27 +0300

    makeitdark

--- makeitdark/flutter_service_worker.js
+++ makeitdark/flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ebbde2005f42fcc5832ec476c15db79f",
-"/": "ebbde2005f42fcc5832ec476c15db79f",
-"main.dart.js": "a7f4cc585631feb94b6a65efcd282fd2",
+"index.html": "4d978712e2d2f37779fa43354544a3ad",
+"/": "4d978712e2d2f37779fa43354544a3ad",
+"main.dart.js": "811aa09113b7a5b3083b2c86cbecda88",
 "manifest.json": "7dbe07db07a28a79aa24716c7284e54b",
 "version.json": "df084fc5e85693af376872960de2b0ca"
 };
