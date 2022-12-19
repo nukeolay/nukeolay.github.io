d1cb53be8610b1a9c16471d2387f679dfec833e9
nukeolay <66204571+nukeolay@users.noreply.github.com>
2022-12-19 22:57:08 +0300

    added ddd

--- ddd/flutter_service_worker.js
+++ ddd/flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2082a654ef4d030b8a10d5199bf29ca5",
-"/": "2082a654ef4d030b8a10d5199bf29ca5",
-"main.dart.js": "8f390ccaab514b800042bff4372746c7",
+"index.html": "6ec4047f93a989a11af863c62fff534e",
+"/": "6ec4047f93a989a11af863c62fff534e",
+"main.dart.js": "dca3b0388b4b5de006b83d061c810712",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
