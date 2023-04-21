875107e35ff55dcdbba273d098970df802dced30
nukeolay <66204571+nukeolay@users.noreply.github.com>
2023-04-21 20:03:20 +0300

    updated dds

--- dds/flutter_service_worker.js
+++ dds/flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c0ca291944b9a84fec8ef4416aff6e59",
-"/": "c0ca291944b9a84fec8ef4416aff6e59",
-"main.dart.js": "4d7d76f658ed6a58e85a21a201fbec98",
+"index.html": "563b4833ab0b4cd4fd1c932272ab6cad",
+"/": "563b4833ab0b4cd4fd1c932272ab6cad",
+"main.dart.js": "fa3bb8670a2c56332f2e4b3eceaf2bcd",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
