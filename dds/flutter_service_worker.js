22c5af44356b0b177de5597f58f43c2be0eb7d00
nukeolay <66204571+nukeolay@users.noreply.github.com>
2023-06-06 12:12:40 +0300

    updated dds

--- dds/flutter_service_worker.js
+++ dds/flutter_service_worker.js
@@ -37,9 +37,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "4c76ef490a105a7921648a0dbe83fe55
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c87fc24462b7b6e151302414f4561e63",
-"/": "c87fc24462b7b6e151302414f4561e63",
-"main.dart.js": "0bc5781dd82a4e91037a52ebdb9c3a58",
+"index.html": "2bbcac5d617f89085a02ca4af1d5a3cb",
+"/": "2bbcac5d617f89085a02ca4af1d5a3cb",
+"main.dart.js": "418fa0c9af88b06f4b334d1c96f931ac",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "ff966ab969ba381b900e61629bfb9789"};
 // The application shell files that are downloaded before a service worker can
